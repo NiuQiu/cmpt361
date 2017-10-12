d3f36da7f3bdf5819fdd4347271e5198e65a0732
hong qiu <qiuandhong4@gmail.com>
2017-10-12 10:42:10 -0700

    second version wu's algorithm

--- line/AntialiasingLineRenderer.java
+++ line/AntialiasingLineRenderer.java
@@ -26,38 +26,56 @@ public class AntialiasingLineRenderer implements LineRenderer {
             finalSlope = 1.0;
         }
 
-        // handle start point
+
         double initX = round(currentXValue);
         double initY = integerPart(currentYValue + slope * (initX - currentXValue));
         double fractional = fractionPart(initY);
         double xGap = remainFractionPart(p1.getX() + 0.5);
+
+
         double rFractional = remainFractionPart(currentYValue * xGap);
 
+        // handle start point
         if (finalSlope <= MIDANGLE) {
             panel.setPixel((int) initX, integerPart(initY), 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-            panel.setPixel((int) initX, integerPart(initY) + 1, 0.0, adjustOpacity(originalColor, fractional).asARGB());
+            panel.setPixel((int) initX, integerPart(initY) - 1, 0.0, adjustOpacity(originalColor, fractional).asARGB());
         } else {
             panel.setPixel((int) initX, (int) initY, 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-            panel.setPixel((int) initX + 1, (int) initY, 0.0, adjustOpacity(originalColor, fractional).asARGB());
+            panel.setPixel((int) initX - 1, (int) initY, 0.0, adjustOpacity(originalColor, fractional).asARGB());
         }
         currentYValue += finalSlope;
 
+        // handle end point
+        initX = round(p2.getX());
+        initY = integerPart(p2.getY() + slope * (initX - p2.getX()));
+        fractional = fractionPart(initY);
+        xGap = remainFractionPart(p2.getX() + 0.5);
+        rFractional = remainFractionPart(p2.getY() * xGap);
         if (finalSlope <= MIDANGLE) {
-            for (int x = p1.getIntX() + 1; x <= p2.getIntX(); x++) {
+            panel.setPixel((int) initX, integerPart(initY), 0.0, adjustOpacity(originalColor, rFractional).asARGB());
+            panel.setPixel((int) initX, integerPart(initY) - 1, 0.0, adjustOpacity(originalColor, rFractional).asARGB());
+        } else {
+            panel.setPixel((int) initX, (int) initY, 0.0, adjustOpacity(originalColor, rFractional).asARGB());
+            panel.setPixel((int) initX - 1, (int) initY, 0.0, adjustOpacity(originalColor, rFractional).asARGB());
+        }
+
+        // handle midpoints
+        if (finalSlope <= MIDANGLE) {
+            for (int x = p1.getIntX() + 1; x <= p2.getIntX()-1; x++) {
                 fractional = fractionPart(currentYValue);
                 rFractional = remainFractionPart(currentYValue);
                 panel.setPixel(x, integerPart(currentYValue), 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-                panel.setPixel(x, integerPart(currentYValue) + 1, 0.0, adjustOpacity(originalColor, fractional).asARGB());
+                panel.setPixel(x, integerPart(currentYValue) - 1, 0.0, adjustOpacity(originalColor, fractional).asARGB());
                 currentYValue += finalSlope;
             }
 
         } else {
-            for (int y = p1.getIntY() + 1; y <= p2.getIntY(); y++) {
+            for (int y = p1.getIntY() + 1; y <= p2.getIntY()-1; y++) {
                 fractional = fractionPart(currentXValue);
                 rFractional = remainFractionPart(currentXValue);
 
                 panel.setPixel(integerPart(currentXValue), y, 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-                panel.setPixel(integerPart(currentXValue) + 1, y, 0.0, adjustOpacity(originalColor, fractional).asARGB());
+                panel.setPixel(integerPart(currentXValue) - 1, y, 0.0, adjustOpacity(originalColor, fractional).asARGB());
                 currentXValue += finalSlope;
             }
 

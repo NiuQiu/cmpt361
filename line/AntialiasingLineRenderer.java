4cead31659e7f5ec630bb4b4eee8f0999189a283
unknown <qiuandhong4@gmail.com>
2017-10-14 16:01:59 -0700

    finish antialiasing alogrithm

--- line/AntialiasingLineRenderer.java
+++ line/AntialiasingLineRenderer.java
@@ -3,82 +3,39 @@ package line;
 import geometry.Vertex3D;
 import line.helper.Supplier;
 import windowing.drawable.Drawable;
-import windowing.graphics.Color;
-
-import static java.lang.Math.round;
+import static java.lang.Math.abs;
 
 /**
- * Created by Administrator on 10/3/2017.
+ * @author Administrator on
+ * @date 10/3/2017.
  */
 public class AntialiasingLineRenderer implements LineRenderer {
-    private static final int MIDANGLE = 1;
+    private final static double RADIUS = Math.sqrt(2);
+    private final static double RADIUSSQUARED = Math.pow(Math.sqrt(2), 2);
+    private final static double CIRCLEAREA = Math.PI * RADIUSSQUARED;
+    private final static int MAXDISTANCE = 1;
+    private final static int ANTIALIAS_RANGE = 3;
 
+    // If this value is 1 then there is no change
+    // If < 1 then the coverage is increased (Aliasing starts to show around 0.5)
+    // If > 1 then coverage is decreased (Image is nearly invisible at 1.6)
+    // Does not increase coverage of 0
+    private static final double COVERAGE_CHANGE = 1;
 
     @Override
     public void drawLine(Vertex3D p1, Vertex3D p2, Drawable panel) {
         double slope = Supplier.calculateSlope(p1, p2);
-        double finalSlope = Supplier.calculateAbsValue(slope);
-        Color originalColor = p1.getColor();
-        double currentYValue = p1.getY();
-        double currentXValue = p1.getX();
+        double y = p1.getY();
+        int color = p1.getColor().asARGB();
+        double coverage;
 
-        if (Supplier.calculateDeltaX(p1, p2) == 0.0) {
-            finalSlope = 1.0;
-        }
-
-
-        double initX = round(currentXValue);
-        double initY = integerPart(currentYValue + slope * (initX - currentXValue));
-        double fractional = fractionPart(initY);
-        double xGap = remainFractionPart(p1.getX() + 0.5);
-
-
-        double rFractional = remainFractionPart(currentYValue * xGap);
-
-        // handle start point
-        if (finalSlope <= MIDANGLE) {
-            panel.setPixel((int) initX, integerPart(initY), 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-            panel.setPixel((int) initX, integerPart(initY) - 1, 0.0, adjustOpacity(originalColor, fractional).asARGB());
-        } else {
-            panel.setPixel((int) initX, (int) initY, 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-            panel.setPixel((int) initX - 1, (int) initY, 0.0, adjustOpacity(originalColor, fractional).asARGB());
-        }
-        currentYValue += finalSlope;
-
-        // handle end point
-        initX = round(p2.getX());
-        initY = integerPart(p2.getY() + slope * (initX - p2.getX()));
-        fractional = fractionPart(initY);
-        xGap = remainFractionPart(p2.getX() + 0.5);
-        rFractional = remainFractionPart(p2.getY() * xGap);
-        if (finalSlope <= MIDANGLE) {
-            panel.setPixel((int) initX, integerPart(initY), 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-            panel.setPixel((int) initX, integerPart(initY) - 1, 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-        } else {
-            panel.setPixel((int) initX, (int) initY, 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-            panel.setPixel((int) initX - 1, (int) initY, 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-        }
-
-        // handle midpoints
-        if (finalSlope <= MIDANGLE) {
-            for (int x = p1.getIntX() + 1; x <= p2.getIntX()-1; x++) {
-                fractional = fractionPart(currentYValue);
-                rFractional = remainFractionPart(currentYValue);
-                panel.setPixel(x, integerPart(currentYValue), 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-                panel.setPixel(x, integerPart(currentYValue) - 1, 0.0, adjustOpacity(originalColor, fractional).asARGB());
-                currentYValue += finalSlope;
+        for (int x = p1.getIntX(); x <= p2.getIntX(); x++){
+            for (int pixelY = (int) (Math.round(y) - ANTIALIAS_RANGE); pixelY <= Math.round(y) + ANTIALIAS_RANGE; pixelY++){
+                coverage = calculatePixelCoverage(p1, p2, x, pixelY);
+                if (coverage != 0){ coverage = 1 - COVERAGE_CHANGE*(1-coverage);}
+                panel.setPixelWithCoverage(x, pixelY, 0.0, color, coverage);
             }
-
-        } else {
-            for (int y = p1.getIntY() + 1; y <= p2.getIntY()-1; y++) {
-                fractional = fractionPart(currentXValue);
-                rFractional = remainFractionPart(currentXValue);
-
-                panel.setPixel(integerPart(currentXValue), y, 0.0, adjustOpacity(originalColor, rFractional).asARGB());
-                panel.setPixel(integerPart(currentXValue) - 1, y, 0.0, adjustOpacity(originalColor, fractional).asARGB());
-                currentXValue += finalSlope;
-            }
-
+            y += slope;
         }
 
     }
@@ -87,23 +44,85 @@ public class AntialiasingLineRenderer implements LineRenderer {
         return new AnyOctantLineRenderer(new AntialiasingLineRenderer());
     }
 
-    private int integerPart(double x) {
-        return (int) x;
+    /**
+     * This function use dot products to calculate the distance
+     * for {v1[p1, (x,y)], v2[p1, p2]} and {v1, v3}
+     * Detail:
+     *
+     * -- Step 1 --
+     * v1*v2 = cos(theta) * |v1| * |v2| (dot product)
+     * cos(theta) = v3/v1 (adjacent/hypotenuse)
+     * -> v3/v1 = (v1*v2) / (|v1|*|v2|)
+     * -> v3 = v2 / |v1|*|v2|
+     * -> v3/v2 = v2 / (|v1|*|v2|)^2 (because deltaX^2 + deltaY^2 = v2^2)
+     * -> ratio = v3/v2
+     *
+     * -- Step 2 --
+     * orthogonalX = p1.getX + ratio*deltaX (scale down x value)
+     * orthogonalY = p1.getY + ratio*deltaY (same reason above)
+     * Then use pythagorean to calculate distance, distance must
+     * within the range of p1 and p2
+     *
+     * @param x x is the current x value
+     * @param y y is the current y value
+     * @return the distance from pixel center point to (orthogonalX, orthogonalY) on vector(p1, p2)
+     */
+    private double getDistance(Vertex3D p1, Vertex3D p2, double x, double y){
+        double orthogonalX, orthogonalY;
+
+        // hypotenuse and opposite for [p1, (x,y)]
+        double A = x - p1.getX();
+        double B = y - p1.getY();
+        // hypotenuse and opposite for [p1, p2]
+        double C = Supplier.calculateDeltaX(p1, p2);
+        double D = Supplier.calculateDeltaY(p1, p2);
+
+        // See step 1
+        double dotProduct = A*C + B*D;
+        double hypotenuse = C*C + D*D;
+        double ratio = -1;
+
+        if (hypotenuse != 0){
+            ratio = dotProduct / hypotenuse;
+        }
+
+        // see step 2
+        if (ratio < 0){
+            orthogonalX = p1.getX();
+            orthogonalY = p1.getY();
+        } else if (ratio > 1){
+            orthogonalX = p2.getX();
+            orthogonalY = p2.getY();
+        } else {
+            orthogonalX = p1.getX() + ratio *C;
+            orthogonalY = p1.getY() + ratio *D;
+        }
+
+        return Math.sqrt(Math.pow(abs(x - orthogonalX), 2) + Math.pow(abs(y - orthogonalY), 2));
     }
 
-    private double fractionPart(double x) {
-        return x - integerPart(x);
+    private double getAngle(double distance){
+        return Math.acos(distance / RADIUS);
     }
 
-    private double remainFractionPart(double x) {
-        return 1.0 - fractionPart(x);
+    private double calculatePixelCoverage(Vertex3D p1, Vertex3D p2, int x, int y){
+        double distance = getDistance(p1, p2, x, y);
+        if (distance > MAXDISTANCE) {return 0;}
+        double angle = getAngle(distance);
+        double numerator =  calculatePieWedge(angle) + calculateTriangle(distance);
+        return 1 - (numerator / CIRCLEAREA);
     }
 
-    private Color adjustOpacity(Color color, double fractional) {
-        double r = color.getR() * (1 - fractional);
-        double g = color.getG() * (1 - fractional);
-        double b = color.getB() * (1 - fractional);
-
-        return new Color(r, g, b);
+    private double calculateTriangle(double distance){
+        return distance*Math.sqrt(RADIUSSQUARED - Math.pow(distance, 2));
     }
+
+    private double calculatePieWedge(double angle){
+        double radian = angle/Math.PI;
+        return (1 - radian)* CIRCLEAREA;
+    }
+
+
+
+
 }
